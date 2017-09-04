b8e8ee560236d0f276e637c4718cc822110e903a
Dolewski Damian <damian.dolewski@tieto.com>
2017-09-04 15:30:20 +0200

    Import biblioteki Onion; Serwer WWW

--- ster_linux/src/boxer_orangePi.c
+++ ster_linux/src/boxer_orangePi.c
@@ -12,10 +12,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include <onion/onion.h>
+#include <onion/handler.h>
+#include <onion/log.h>
+
+#include <onion/exportlocal.h>
+#include <onion/static.h>
+#include <onion/shortcuts.h>
+
 #include "uart.h"
 #include "database.h"
 
 #define TEST_DB_INSERT_STATEMENT
+
+onion * o = NULL;
+
+void free_onion(int unused){
+	ONION_INFO("Closing connections");
+	onion_free(o);
+	exit(0);
+}
 
 int main(void)
 {
@@ -41,10 +60,34 @@ int main(void)
 	DataBase_InsertPhMeas(&testPhMeas);
 #endif
 
+	char *port="8080";
+	const char *dirname="/home/dolewdam/Git_Repos/Prywatne/boxer_orangepi_www/website/source";
+
+	onion_handler *dir=onion_handler_export_local_new(dirname);
+	onion_handler_add(dir, onion_handler_static("<h1>404 - File not found.</h1>", 404) );
+
+	o=onion_new(O_THREADED|O_DETACH_LISTEN|O_SYSTEMD);
+	onion_set_root_handler(o, dir);
+
+
+	onion_set_port(o, port);
+
+	signal(SIGINT, free_onion);
+	int error=onion_listen(o);
+	if (error){
+		perror("Cant create the server");
+	}
+	ONION_INFO("Detached. Printing a message every 120 seconds.");
+	int i=0;
+
 	while (true)
 	{
 		Uart_RxHandler();
+		i++;
+		ONION_INFO("Still alive. Count %d.",i);
+		sleep(120);
 	}
 
+	onion_free(o);
 	return EXIT_SUCCESS;
 }
